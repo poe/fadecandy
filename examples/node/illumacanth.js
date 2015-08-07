f42be7e3977b4f18d041d504d4e68bcf965aeb54
Paul Showalter <poeslacker@gmail.com>
2015-08-07 16:16:09 -0700

    added layouts

--- examples/node/illumacanth.js
+++ examples/node/illumacanth.js
@@ -1,8 +1,8 @@
 var express = require('express');
- 
+
 var server = express();
 server.use(express.static(__dirname + '/public'));
- 
+
 var port = 10001;
 server.listen(port, function() {
     console.log('server listening on port ' + port);
